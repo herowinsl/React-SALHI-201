47f40528f7bdc4252a9506f6f4aa69cf12a3b8d1
herowinsl <iliassalhi1949@gmail.com>
2025-11-15 11:31:50 +0100

    new upadte

---
+++ React.js
@@ -0,0 +1,14 @@
+class Etudiant{
+    constructor(nom,age){
+        this.nom = nom
+        this.age = age
+    }
+    info(){
+        return `Etudinat nom: ${this.nom} a pour age:${this.age}`
+    }
+}
+let et1 = new Etudiant("ilias", 20);
+let et2 = new Etudiant("ilyass", 21);
+console.log(et1);
+console.log(et2);
+
